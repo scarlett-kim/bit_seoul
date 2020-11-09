3084fe4980ebc5056de13e19b48d91c46d43a96a
scareltt-kim <scdeveloper0728@gmail.com>
2020-11-09 20:01:23 +0900

    1일차 수업

---
+++ test/test01.py
@@ -0,0 +1,6 @@
+print('hello world')
+a = 1
+b = 2 
+c = a + b
+print(c)
+
---
+++ test/test03.py
@@ -0,0 +1,12 @@
+a = [1,2,3]
+b = [4,5,6]
+c = a + b
+print(c) #[1, 2, 3, 4, 5, 6]
+print(a + b) #[1, 2, 3, 4, 5, 6]
+
+#[5,7,9]가 나오고 싶다면? 
+import numpy as np
+x = np.array([1, 2, 3]) #array 행렬의 덧셈  
+y = np.array([4, 5, 6]) 
+print(x + y)
+
